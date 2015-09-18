37933a900adbbac0d102f5012d8bf5fe6d420c23
hansu <optu123@naver.com>
2015-09-18 14:09:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hansu/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.hansu.hansuapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    Button button1,button2,button3,button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button1.setBackgroundColor(Color.GRAY);
+        button2.setBackgroundColor(Color.GREEN);
+        button3.setBackgroundColor(Color.RED);
+        button4.setBackgroundColor(Color.YELLOW);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mlntent = new Intent(getIntent().ACTION_VIEW, Uri.parse("http://m.nate.com"));
+                startActivity(mlntent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mlntent = new Intent(getIntent().ACTION_VIEW, Uri.parse("tel:/911"));
+                startActivity(mlntent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mlntent = new Intent(getIntent().ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mlntent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+
+}
